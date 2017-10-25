21c8f4926c8a6339282ba4bb777aa6d2e8afa656
idonans <idonans@126.com>
2017-10-25 16:23:36 +0800

    add butterknife config for module app

--- app/src/main/java/com/okandroid/demo/block/MainActivity.java
+++ app/src/main/java/com/okandroid/demo/block/MainActivity.java
@@ -4,12 +4,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
-  @Override protected void onCreate(Bundle savedInstanceState) {
+  @BindView(R.id.item_content)
+  TextView mItemContent;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    ButterKnife.bind(this);
+
+    mItemContent.setText("hello, butter knife");
   }
 
   public void onClickOtherActivity(View view) {
