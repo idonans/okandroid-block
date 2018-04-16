e6ca73145d7b1fb772cbb27cdecb2ca73ffcf717
idonans <idonans@126.com>
2018-04-16 11:12:16 +0800

    modify AbortUtil isAbort check logic, add UNINTERRUPTED constant

--- block/src/main/java/com/okandroid/block/util/AbortUtil.java
+++ block/src/main/java/com/okandroid/block/util/AbortUtil.java
@@ -7,17 +7,34 @@ import com.okandroid.block.lang.AbortSignal;
 
 public class AbortUtil {
 
+    /**
+     * @param abortSignal
+     * @return true if abortSignal is null or isAbort, otherwise false.
+     */
     public static boolean isAbort(@Nullable AbortSignal abortSignal) {
         if (abortSignal == null) {
-            return false;
+            return true;
         }
         return abortSignal.isAbort();
     }
 
+    /**
+     * @see #isAbort(AbortSignal)
+     */
     public static void throwIfAbort(@Nullable AbortSignal abortSignal) {
         if (isAbort(abortSignal)) {
             throw new AbortException();
         }
     }
+
+    /**
+     * one AbortSingle the method isAbort always return false.
+     */
+    public static final AbortSignal UNINTERRUPTED = new AbortSignal() {
+        @Override
+        public boolean isAbort() {
+            return false;
+        }
+    };
 
 }
